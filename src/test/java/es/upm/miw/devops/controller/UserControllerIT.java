6e2a463770f1c6c8f633eb3519ec71a3f5662c4e
zuribehobide23 <z.behobide@gmail.com>
2026-09-14 18:04:29 +0200

    Fixed GitHub Actions integration tests. Refs #1

--- src/test/java/es/upm/miw/devops/controller/UserControllerIT.java
+++ src/test/java/es/upm/miw/devops/controller/UserControllerIT.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -12,7 +13,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest(classes = Application.class)
 @AutoConfigureMockMvc
-class UserControllerTest {
+@ActiveProfiles("test")
+class UserControllerIT {
 
     @Autowired
     private MockMvc mockMvc;
