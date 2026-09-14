cc153eebc715b64b53f1b153641bc0618a12300a
zuribehobide23 <z.behobide@gmail.com>
2026-09-14 16:25:36 +0200

    Tests for service and endpoint GET /user/{id} finished. Refs #13

---
+++ src/test/java/es/upm/miw/devops/controller/UserControllerTest.java
@@ -0,0 +1,27 @@
+package es.upm.miw.devops.controller;
+
+import es.upm.miw.devops.Application;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest(classes = Application.class)
+@AutoConfigureMockMvc
+class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testGetUserById() throws Exception {
+        mockMvc.perform(get("/user/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.name").value("Zurine"));
+    }
+}
