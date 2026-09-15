e6e7bffb4d603a642923f168fd2e220c0919ad00
zuribehobide23 <z.behobide@gmail.com>
2026-09-15 11:32:29 +0200

    Added billable user filtering. Refs #6

--- src/main/java/es/upm/miw/devops/infrastructure/data/models/User.java
+++ src/main/java/es/upm/miw/devops/infrastructure/data/models/User.java
@@ -10,15 +10,121 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String name;
+    @Column(name = "first_name")
+    private String firstName;
 
-    public User() {}
+    @Column(name = "family_name")
+    private String familyName;
 
-    public User(String name) {
-        this.name = name;
+    private String email;
+
+    private String identity;
+
+    private String address;
+
+    private String city;
+
+    private String province;
+
+    @Column(name = "postal_code")
+    private String postalCode;
+
+    public User() {
     }
 
-    public Long getId() { return id; }
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+    public User(String firstName, String familyName, String email,
+                String identity, String address, String city,
+                String province, String postalCode) {
+        this.firstName = firstName;
+        this.familyName = familyName;
+        this.email = email;
+        this.identity = identity;
+        this.address = address;
+        this.city = city;
+        this.province = province;
+        this.postalCode = postalCode;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFamilyName() {
+        return familyName;
+    }
+
+    public void setFamilyName(String familyName) {
+        this.familyName = familyName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getIdentity() {
+        return identity;
+    }
+
+    public void setIdentity(String identity) {
+        this.identity = identity;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public boolean isBillable() {
+        return hasContent(firstName)
+                && hasContent(familyName)
+                && hasContent(email)
+                && hasContent(identity)
+                && hasContent(address)
+                && hasContent(city)
+                && hasContent(province)
+                && hasContent(postalCode);
+    }
+
+    private boolean hasContent(String value) {
+        return value != null && !value.isBlank();
+    }
 }
